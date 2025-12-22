751781ad5dc5ab2b0c573dc26d629b7fdcb20570
devil <europedreadlydevil@gmail.com>
2025-12-23 00:52:34 +0300

    Add api for use ast nodes

--- src/api/ast/mod.rs
+++ src/api/ast/mod.rs
@@ -12,8 +12,12 @@ impl<'src> Process<'src> {
     pub fn new(ident: Spanned<Ident<'src>>, branches: Vec<Spanned<Branch<'src>>>) -> Process<'src> {
         Self { ident, branches }
     }
+
+    pub fn branches(&self) -> Vec<Branch<'src>> {
+        self.branches.iter().map(|x| x.inner.clone()).collect()
+    }
 }
-#[derive(Debug, PartialEq, PartialOrd)]
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
 pub struct Branch<'src> {
     patterns: Vec<Spanned<Pattern<'src>>>,
     body: Vec<Spanned<Expr<'src>>>,
@@ -26,9 +30,16 @@ impl<'src> Branch<'src> {
     ) -> Branch<'src> {
         Self { patterns, body }
     }
+
+    pub fn patterns(&self) -> Vec<Pattern<'src>> {
+        self.patterns.iter().map(|x| x.inner.clone()).collect()
+    }
+    pub fn body(&self) -> Vec<Expr<'src>> {
+        self.body.iter().map(|x| x.inner.clone()).collect()
+    }
 }
 
-#[derive(Debug, PartialEq, PartialOrd)]
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
 pub struct Pattern<'src> {
     ident: Spanned<Ident<'src>>,
     ty: Spanned<Type<'src>>,
@@ -43,9 +54,21 @@ impl<'src> Pattern<'src> {
     ) -> Pattern<'src> {
         Self { ident, ty, default }
     }
+
+    pub fn has_default(&self) -> bool {
+        self.default.is_some()
+    }
+
+    pub fn ty(&self) -> Type<'src> {
+        self.ty.inner.clone()
+    }
+
+    pub fn ident(&self) -> Ident<'src> {
+        self.ident.inner.clone()
+    }
 }
 
-#[derive(Debug, PartialEq, PartialOrd)]
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
 pub struct Ident<'src>(&'src str);
 impl<'src> Ident<'src> {
     pub fn new(inner: &'src str) -> Ident<'src> {
@@ -53,7 +76,13 @@ impl<'src> Ident<'src> {
     }
 }
 
-#[derive(Debug, PartialEq, PartialOrd)]
+impl ToString for Ident<'_> {
+    fn to_string(&self) -> String {
+        self.0.to_string()
+    }
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
 pub enum Type<'src> {
     Path(Spanned<Ident<'src>>, Spanned<Box<Type<'src>>>),
     Type(Spanned<Ident<'src>>),
